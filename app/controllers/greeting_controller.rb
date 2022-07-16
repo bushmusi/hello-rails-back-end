e24c57ea5060e1ce0482a93d29cf00ce0dd92369
bushmusi <bushera.mestofa@gmail.com>
2022-07-16 10:38:17 +0300

    Fix linter error

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Service to download ftp files from the server
 class GreetingController < ApplicationController
   def index
     @message = Message.all.sample(1)
--- db/migrate/20220715141207_create_messages.rb
+++ db/migrate/20220715141207_create_messages.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Service to download ftp files from the server
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
